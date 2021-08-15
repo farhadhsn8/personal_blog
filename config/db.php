539e237ca942f373a5d96aae59c65039e595e6e1
farhadhsn <farhadhsn8@gmail.com>
2021-08-15 21:28:21 +0430

    init

---
+++ config/db.php
@@ -0,0 +1,16 @@
+<?php
+
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;port=3308;dbname=personal_weblog',
+    'emulatePrepare' =>true,
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
